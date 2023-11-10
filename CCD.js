4611807cdf9c465e1c139c13853e992b2b6bd773
Родион <48344578+radik097@users.noreply.github.com>
2023-11-10 08:25:49 +0100

    Update CCD.js
    Update 0.2

--- CCD.js
+++ CCD.js
@@ -1,82 +1,92 @@
 // ==UserScript==
 // @name         Custom Commands Dropdown
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    pl.4as.chatgpt
+// @version      0.2
 // @description  Add a dropdown of custom commands to a page
-// @author       You
-// @match        *://*/*
+// @author       Description009
+// @match *://chat.openai.com/*
+// @updateURL https://github.com/radik097/Custom-Command-Center/
+// @downloadURL https://github.com/radik097/Custom-Command-Center/
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+"use strict";
 
-    // This function will add our custom commands dropdown
-    function addCustomCommandsDropdown() {
-        // Get the textarea by ID
-        const textareaElement = document.getElementById('prompt-textarea');
+(function () {
+  "use strict";
+  console.log("CCD loading...");
 
-        // If the textarea exists, proceed to add the dropdown
-        if (textareaElement) {
-            // Create the select element
-            const select = document.createElement('select');
-            select.name = 'Commands';
-            select.id = 'commands';
-            select.style.borderTop = 'none';
-            select.className = "m-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 gizmo:md:py-3.5 pl-12 gizmo:pl-10 md:pl-[46px] gizmo:md:pl-[55px]";
-            // List of commands for autocomplete
-            const commands = [
-                "Select Command",
-                "myfiles_browser.search",
-                "myfiles_browser.open_url",
-                "myfiles_browser.back",
-                "myfiles_browser.scroll",
-                "myfiles_browser.quote_lines",
-                "browser.search",
-                "browser.click",
-                "browser.back",
-                "browser.scroll",
-                "browser.open_url",
-                "browser.quote_lines"
-                // ... add any other commands here if necessary
-            ];
+  // This function will add our custom commands dropdown
+  function addCustomCommandsDropdown() {
+    // Get the textarea by ID
+    const textareaElement = document.getElementById("prompt-textarea");
 
-            // Populate the select element with options
-            commands.forEach((command) => {
-                const option = document.createElement('option');
-                console.log(command+" Loaded!");
-                option.value = command;
-                option.textContent = command;
-                option.style.backgroundColor="#343541";
-                option.style.borderBottom='none';
-                select.appendChild(option);
-                });
+    // If the textarea exists, proceed to add the dropdown
+    if (textareaElement) {
+      // Create the select element
+              var select_id=document.getElementById('commands')
+      if (select_id){console.log('menu is loaded')}else{
+      const select = document.createElement("select");
+      select.name = "Commands";
+      select.id = "commands";
+      select.style.borderTop = "none";
+      select.className =
+        "m-0 w-full resize-none border-0 bg-transparent py-[10px] pr-10 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:py-4 md:pr-12 gizmo:md:py-3.5 pl-12 gizmo:pl-10 md:pl-[46px] gizmo:md:pl-[55px]";
 
-            // Insert the select element before the textarea
-            textareaElement.parentNode.insertBefore(select, textareaElement);
+      // List of commands for autocomplete
+      const commands = [
+        "Select Command",
+        "myfiles_browser.search",
+        "myfiles_browser.open_url",
+        "myfiles_browser.back",
+        "myfiles_browser.scroll",
+        "myfiles_browser.quote_lines",
+        "browser.search",
+        "browser.click",
+        "browser.back",
+        "browser.scroll",
+        "browser.open_url",
+        "browser.quote_lines",
+        // ... add any other commands here if necessary
+      ];
 
-            // Event listener for select change
-            select.addEventListener('change', function() {
-                const selectedOption = select.options[select.selectedIndex].value;
-                textareaElement.value += selectedOption + '();\n';
-            });
+      // Populate the select element with options
+      commands.forEach((command) => {
+        const option = document.createElement("option");
+        option.value = command;
+        option.textContent = command;
+        option.style.backgroundColor = "#343541";
+        option.style.borderBottom = "none";
+        select.appendChild(option);
+      });
 
-            console.log('Custom commands dropdown added.');
-        } else {
-            console.error("Textarea not found.");
-        }
-    }
+      // Insert the select element before the textarea
+      textareaElement.parentNode.insertBefore(select, textareaElement);
 
-    // Inject our addCustomCommandsDropdown function into the page
-    function injectFunction(fn) {
-        const script = document.createElement('script');
-        script.text = `(${fn.toString()})();`;
-        document.body.appendChild(script);
-    }
+      // Event listener for select change
+      select.addEventListener("change", function () {
+        const selectedOption = select.options[select.selectedIndex].value;
+        textareaElement.value += selectedOption + "();\n";
+      });
+      }
+      console.log("Custom commands dropdown added.");
+    } else {
+      console.error("Textarea not found.");
+    };
+   setTimeout(addCustomCommandsDropdown,2000);console.log('Run function');
+  }
 
-    // Wait for the page to load
-    window.addEventListener('load', function() {
-        injectFunction(addCustomCommandsDropdown);
-    });
+  // Inject our addCustomCommandsDropdown function into the page
+  function injectFunction(fn) {
+    const script = document.createElement("script");
+    script.text = `(${fn.toString()})();`;
+      script.id = 'CustomCommand'
+    document.body.appendChild(script);
+    console.log("Code injected using injectionFunc");
+  }
 
+  // Wait 10 seconds after the page loads before injecting the dropdown
+  setTimeout(function () {
+    injectFunction(addCustomCommandsDropdown);
+  }, 1000); // 1 seconds expressed in milliseconds
 })();
